a44e6076d3912bb1b532c349e56caa8c36ba3d02
Yaswanth Sirigiri <93373615+yaswanths2002@users.noreply.github.com>
2023-11-21 11:55:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+const { ApolloServer } = require("apollo-server"); // Import Apollo Server
+const { importSchema } = require("graphql-import"); // Import graphql-import to load schema
+const EtherDataSource = require("./datasource/ethDatasource"); // Import custom data source
+
+const typeDefs = importSchema("./schema.graphql"); // Load schema
+
+require("dotenv").config(); // Load environment variables
+
+const resolvers = {
+  Query: {
+    // Resolver for etherBalanceByAddress query
+    etherBalanceByAddress: (root, _args, { dataSources }) =>
+      dataSources.ethDataSource.etherBalanceByAddress(),
+
+    // Resolver for totalSupplyOfEther query
+    totalSupplyOfEther: (root, _args, { dataSources }) =>
+      dataSources.ethDataSource.totalSupplyOfEther(),
+
+    // Resolver for latestEthereumPrice query
+    latestEthereumPrice: (root, _args, { dataSources }) =>
+      dataSources.ethDataSource.getLatestEthereumPrice(),
+
+    // Resolver for blockConfirmationTime query
+    blockConfirmationTime: (root, _args, { dataSources }) =>
+      dataSources.ethDataSource.getBlockConfirmationTime(),
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  dataSources: () => ({
+    ethDataSource: new EtherDataSource(), // Instantiate EtherDataSource
+  }),
+});
+
+server.timeout = 0;
+
+server.listen("9000").then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
